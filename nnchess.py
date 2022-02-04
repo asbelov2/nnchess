002471c45b056a4fdae15008753f0399bfd1fb1c
asbelov2 <37810516+asbelov2@users.noreply.github.com>
2022-02-04 18:19:20 +0400

    first commit

---
+++ nnchess.py
@@ -0,0 +1,96 @@
+import chess
+import chess.engine
+import random
+import numpy
+from datetime import datetime
+from datetime import timedelta
+
+def random_board(max_depth=200):
+  board = chess.Board()
+  depth = random.randrange(0, max_depth)
+
+  for _ in range(depth):
+    all_moves = list(board.legal_moves)
+    random_move = random.choice(all_moves)
+    board.push(random_move)
+    if board.is_game_over():
+      break
+
+  return board
+
+def stockfish(board, depth):
+  with chess.engine.SimpleEngine.popen_uci('content/stockfish.exe') as sf:
+    result = sf.analyse(board, chess.engine.Limit(depth=depth))
+    score = result['score'].white().score()
+    return score
+
+squares_index = {
+  'a': 0,
+  'b': 1,
+  'c': 2,
+  'd': 3,
+  'e': 4,
+  'f': 5,
+  'g': 6,
+  'h': 7
+}
+
+# example: h3 -> 17
+def square_to_index(square):
+  letter = chess.square_name(square)
+  return 8 - int(letter[1]), squares_index[letter[0]]
+
+
+def split_dims(board):
+  # this is the 3d matrix
+  board3d = numpy.zeros((14, 8, 8), dtype=numpy.int8)
+
+  # here we add the pieces's view on the matrix
+  for piece in chess.PIECE_TYPES:
+    for square in board.pieces(piece, chess.WHITE):
+      idx = numpy.unravel_index(square, (8, 8))
+      board3d[piece - 1][7 - idx[0]][idx[1]] = 1
+    for square in board.pieces(piece, chess.BLACK):
+      idx = numpy.unravel_index(square, (8, 8))
+      board3d[piece + 5][7 - idx[0]][idx[1]] = 1
+
+  # add attacks and valid moves too
+  # so the network knows what is being attacked
+  aux = board.turn
+  board.turn = chess.WHITE
+  for move in board.legal_moves:
+      i, j = square_to_index(move.to_square)
+      board3d[12][i][j] = 1
+  board.turn = chess.BLACK
+  for move in board.legal_moves:
+      i, j = square_to_index(move.to_square)
+      board3d[13][i][j] = 1
+  board.turn = aux
+
+  return board3d
+
+
+start_time = datetime.now()
+# returns the elapsed milliseconds since the start of the program
+def millis():
+   dt = datetime.now() - start_time
+   ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+   return ms
+
+def create_dataset(name, dataset_size):
+  i = 0
+  b = []
+  v = []
+  millis()
+  while i < dataset_size:
+    board = random_board()
+    value = stockfish(board, 10)
+    b.append(board)
+    v.append(value)
+    if (i%10==0):
+      t=millis()
+      print(f'{i}/{dataset_size} time {t}')
+    i+=1
+  numpy.savez(f'content/{name}', b, v)
+  
+create_dataset('ds1', 1000000)
